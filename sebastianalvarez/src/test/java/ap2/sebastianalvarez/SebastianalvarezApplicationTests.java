40855bef516b6bb1beb24099ee9a3978ccd61a13
SebastianAlvarezTito <sebastian.alvarez@vallegrande.edu.pe>
2026-03-28 20:01:52 -0500

    feat: implement Customer CRUD and sync with SQL Server via Docker

---
+++ sebastianalvarez/src/test/java/ap2/sebastianalvarez/SebastianalvarezApplicationTests.java
@@ -0,0 +1,13 @@
+package ap2.sebastianalvarez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebastianalvarezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
